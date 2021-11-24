cf50ea85e4705919cb76cb0cbb86d6397563e463
mnecas <necas.marty@gmail.com>
2021-11-24 22:32:31 +0100

    add communication for http and https

--- honeypot/communication.py
+++ honeypot/communication.py
@@ -5,12 +5,44 @@ import json
 import os
 
 
-
 def get_args():
-    parser = argparse.ArgumentParser(description='Python honeypot communication')
+    parser = argparse.ArgumentParser(
+        description='Python honeypot communication')
     parser.add_argument('--server-ip')
     parser.add_argument('--prod', action='store_true')
     return parser.parse_args()
+
+
+def get_http_log():
+    if os.path.exists(configuration.settings.http_log):
+        resp = []
+        with open(configuration.settings.http_log, 'r') as f:
+            print(configuration.settings.http_log)
+            for i in f.readlines():
+                sp = i.split(';')
+                resp.append({
+                    'ip': sp[0],
+                    'text': ';'.join(sp[1:]).rstrip(),
+                })
+        # Cleanup data from file
+        # open(configuration.settings.http_log, 'w').close()
+        return json.dumps(resp)
+
+
+def get_https_log():
+    if os.path.exists(configuration.settings.https_log):
+        resp = []
+        with open(configuration.settings.https_log, 'r') as f:
+            print(configuration.settings.https_log)
+            for i in f.readlines():
+                sp = i.split(';')
+                resp.append({
+                    'ip': sp[0],
+                    'text': ';'.join(sp[1:]).rstrip(),
+                })
+        # Cleanup data from file
+        # open(configuration.settings.http_log, 'w').close()
+        return json.dumps(resp)
 
 
 def get_ssh_log():
@@ -21,9 +53,9 @@ def get_ssh_log():
             for i in f.readlines():
                 sp = i.split(';')
                 resp.append({
-                    'ip':sp[0],
-                    'username':sp[1],
-                    'password':';'.join(sp[2:]).rstrip(),
+                    'ip': sp[0],
+                    'username': sp[1],
+                    'password': ';'.join(sp[2:]).rstrip(),
                 })
         # Cleanup data from file
         # open(configuration.settings.ssh_log, 'w').close()
@@ -40,9 +72,16 @@ if __name__ == "__main__":
     else:
         print("Server started in dev mode.")
         configuration.init('dev')
-    url = f"{args.server_ip}/api/ssh"
-    data = get_ssh_log()
-    headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
-    print(data)
-    x = requests.post(url, data=json.dumps(data), headers=headers)
-    print("resp", x.text)
+    send_list = {
+        'ssh': get_ssh_log,
+        'http': get_http_log,
+        'https': get_https_log,
+    }
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    for key, send_fce in send_list.items():
+        url = f"http://{args.server_ip}/api/{key}"
+        print("URL:", url)
+        data = send_fce()
+        x = requests.post(url, data=json.dumps(data), headers=headers)
+        print("Resp:", x.text)
