08f76c3b2e359e63e67a1efbca5bfc035e731c7b
Mousero141 <Grafy06@gmail.com>
2021-11-24 20:44:40 +0100

    test

--- web/smth/routes.py
+++ web/smth/routes.py
@@ -1,5 +1,10 @@
 from __main__ import app
 from flask import Blueprint, render_template, request
+from smth import db
+from .models import SSHLog
+import time
+
+
 
 @app.route('/', methods=['GET'])  # HOME PAGE
 def home():
@@ -19,3 +24,34 @@ def http():
 @app.route('/https', methods=['GET'])  # HTTPS PAGE
 def https():
     return render_template("https.html")
+
+
+@app.route('/api/ssh', methods=['GET', 'POST'])  # API-SSH ENDPOINT PAGE
+def api_ssh():
+    if request.method == 'POST':
+        print(request.json)
+        for loge in request.json:
+            ip = loge.get("ip") 
+            user = loge.get("username")
+            pswd = loge.get("password")
+            created_time = time.time()
+            login = SSHLog(user, ip, pswd, created_time)
+            db.session.add(login)
+            db.commit()
+        return request.json
+    else:
+        resp = SSHLog.query.all()
+        print(resp)
+
+    #return render_template("api/ssh.html", values=log.query.all())
+    
+
+
+@app.route('/api/http', methods=['GET', 'POST'])  # API-HTTP ENDPOINT PAGE
+def api_http():
+    return "WORKING ON THAT"
+
+
+@app.route('/api/https', methods=['GET', 'POST'])  # API-HTTPS ENDPOINT PAGE
+def api_https():
+    return "WORKING ON THAT"
